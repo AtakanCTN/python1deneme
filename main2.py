0f7d3285b1a3e827615db75fcab85b5e763b8395
Atakan <77112961+AtakanCTN@users.noreply.github.com>
2023-04-11 17:36:03 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,2 +1,10 @@
-def cikartma(x,y):
-    return x-y
+liste = []                                      # 2 ILE 100 SAYI ARALIGINDAKI ASAL SAYILARI YAZAR.
+
+for num in range(2,101):
+    x = True
+    for i in range(2,num):
+        if num % i == 0:
+            x = False
+    if x :
+        liste.append(num)
+print(liste)
